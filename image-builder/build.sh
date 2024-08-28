8dee4cb7f916f0ebe512cd81e6abcdaa518d099e
ApophisLee <ApophisLee@users.noreply.github.com>
2024-08-28 18:09:09 +0800

    Refactor build.sh script for image builder

--- image-builder/build.sh
+++ image-builder/build.sh
@@ -1,4 +1,4 @@
 #bin/bash
 make CONFIG=cloud bin/ipxe.lkrn bin-x86_64-efi/ipxe.efi
 make CONFIG=cloud bin-arm64-efi/ipxe.efi CROSS=aarch64-linux-gnu-
-./util/genfsimg -o ../ipxe.iso -s ../autoexec.ipxe bin/ipxe.lkrn bin-x86_64-efi/ipxe.efi bin-arm64-efi/ipxe.efi
+./util/genfsimg -o bin/ipxe.iso -s autoexec.ipxe bin/ipxe.lkrn bin-x86_64-efi/ipxe.efi bin-arm64-efi/ipxe.efi
